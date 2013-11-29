940403e9a2bac4190cae726c770c3eb61319ac1f
irasally <irasally@gmail.com>
2013-11-29 13:29:13 +0900

    モデルにメソッドを追加

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@
     defaults:{
       title: 'do something!',
       completed: false
+    },
+    toggle : function(){
+      this.set('completed', !this.get('completed'));
     }
   });
   var task1 = new Task({
     title: 'do it!!',
     completed: true
   });
-  console.log(task1);
+  console.log(task1.toJSON());
+  task1.toggle();
+  console.log(task1.toJSON());
   var task2 = new Task();
   task2.set('title', 'new Title');
   var title = task2.get('title');
