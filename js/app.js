60801fe17f00a398cfedf6cbe882902ca4315314
irasally <irasally@gmail.com>
2013-11-29 15:09:44 +0900

    Collectionを使う準備

--- js/app.js
+++ js/app.js
@@ -12,22 +12,12 @@
   var TaskView = Backbone.View.extend({
     tagName: 'li',
     className: 'taskClass',
-    events: {
-      'click .command': 'sayHello'
-    },
-    sayHello: function(){
-      alert('hello');
-    },
     template: _.template($('#task-template').html()),
     render: function(){
       var template = this.template(this.model.toJSON());
       this.$el.html(template);
       return this;
     }
-
   });
-  var taskView = new TaskView({model: task}); // model instance
-  console.log(taskView.render().el);
-  $('body').append(taskView.render().el);
 
 })();
