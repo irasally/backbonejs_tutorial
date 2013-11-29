ab19ed20eae52cef8c19b107f544d6a3bc953ce6
irasally <irasally@gmail.com>
2013-11-29 13:55:12 +0900

    Viewを作る準備

--- js/app.js
+++ js/app.js
@@ -5,27 +5,6 @@
       title: 'do something!',
       completed: false
     },
-    validate : function(attrs){
-      if(_.isEmpty(attrs.title)){
-        return 'Title must not be empty!';
-      }
-    },
-    toggle : function(){
-      this.set('completed', !this.get('completed'));
-    }
   });
-  var task1 = new Task({
-    title: 'do it!!',
-    completed: true
-  });
-  console.log(task1.toJSON());
-  task1.toggle();
-  console.log(task1.toJSON());
-  var task2 = new Task();
-  task2.set('title', 'new Title');
-  var title = task2.get('title');
-  console.log(title);
-  console.log(task2.toJSON());
-  task2.set({title: ''}, {validate: true});
-  console.log(task2.toJSON());
+  var task = new Task();
 })();
