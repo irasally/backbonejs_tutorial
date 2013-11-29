6bd6403b2f30c7e46d1136f7db1a3d016be1ca86
irasally <irasally@gmail.com>
2013-11-29 13:26:38 +0900

    モデルを作成する

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 (function(){
-});
+  // Model
+  var Task = Backbone.Model.extend({
+    defaults:{
+      title: 'do something!',
+      completed: false
+    }
+  });
+  var task1 = new Task({
+    title: 'do it!!',
+    completed: true
+  });
+  console.log(task1);
+  var task2 = new Task();
+  task2.set('title', 'new Title');
+  var title = task2.get('title');
+  console.log(title);
+})();
